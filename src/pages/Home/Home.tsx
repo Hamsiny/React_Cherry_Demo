712d917d22f7ded36d0df305275334ad21665fc4
Hamsiny <459516309@qq.com>
2021-08-12 22:59:01 +1200

    update home page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,14 +1,19 @@
-import React, { Component } from 'react'
-import MediaCard from '../../components/MediaCard/MediaCard'
+import React, { Component } from "react";
+import MediaCard from "../../components/MediaCard/MediaCard";
 
 export class Home extends Component {
-    render() {
-        return (
-            <div className="d-flex justify-content-center">
-                <MediaCard />
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="d-flex justify-content-center">
+        <div>
+          <h3 className="text-center mb-3">
+            <strong>This is Home Page</strong>
+          </h3>
+          <MediaCard />
+        </div>
+      </div>
+    );
+  }
 }
 
-export default Home
+export default Home;
